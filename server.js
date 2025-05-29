bc5a650cf13014ff46f763ffa5a39809d0d9d63c
Suresh <sureshvelpula.342@outlook.com>
2025-05-29 09:02:48 +0530

    adding files into repo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Welcome to AKS private cluster !!</h1> \n');
+  res.send('<h1 style="color:red;">Welcome to Devops class New Batch !!</h1> \n');
 });
 
 app.listen(PORT, HOST);
