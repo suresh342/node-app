deb20b45c01b00e22dcbe395b4104f733c9e92a2
Suresh <sureshvelpula.342@outlook.com>
2025-08-27 20:16:44 +0530

    updated server.js file

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:red;">Welcome to Devops class New Batch !!</h1> \n');
+  res.send('<h1 style="color:red;">Hi welcome to Devops !!</h1> \n');
 });
 
 app.listen(PORT, HOST);
